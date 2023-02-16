36c070eb69d64d3cea66b8169f0fa8ca24c9f2d2
Eduardo Anguiano <marcos.2000.mg15@gmail.com>
2023-02-16 14:43:41 -0600

    integracion de la tabla

---
+++ app/Controllers/Admin/CredencialController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
+
+class CredencialController extends BaseController
+{
+    public function index()
+    {
+        return view("Admin/table");
+    }
+
+    public function new(){
+
+    }
+}
